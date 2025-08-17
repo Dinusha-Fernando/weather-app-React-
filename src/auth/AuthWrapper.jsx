ecd88c9467fac095aadc463cb68b53fd84d9200f
Dinusha Fernando <dinushafdo9998@gmail.com>
2025-08-17 16:42:20 +0530

    Fix Auth0 callback URL handling for dev and production

--- src/auth/AuthWrapper.jsx
+++ src/auth/AuthWrapper.jsx
@@ -5,6 +5,13 @@ import { useAuth0 } from '@auth0/auth0-react';
 const AuthWrapper = ({ children }) => {
     const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0();
 
+
+    const redirectUri =
+        process.env.NODE_ENV === 'development'
+            ? 'http://localhost:5173/login'
+            : 'https://weather-app-react-five-alpha.vercel.app/login';
+
+
     React.useEffect(() => {
         if (!isLoading && !isAuthenticated) {
             loginWithRedirect();
